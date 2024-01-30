fccc5d5c359eaedda9351cde5e082d7ddc703850
Andrew Lee <andrew.lee.cal@gmail.com>
2024-01-30 13:42:01 +0000

    Pipeline upload demo

--- test.sh
+++ test.sh
@@ -1,1 +1,11 @@
-echo "Test test test"
+buildkite-agent pipeline upload <<NEW_STEPS
+steps:
+  - command: "echo Simulating flying"
+    label: "Simulation Test 1"
+  - command: "echo Simulating landing"
+    label: "Simulation Test 2"
+  - command: "echo Run Selenium scenario 1"
+    label: "Web App Test 1"
+  - command: "echo Run Selenium scenario 2"
+    label: "Web App Test 2"
+NEW_STEPS
